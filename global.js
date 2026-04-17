0fd88134acb44a0f3b6dc661ed2424213cbb75a8
sleep1bo1 <rluutuyen@ucsd.edu>
2026-04-17 12:50:39 -0700

    perhaps?

--- global.js
+++ global.js
@@ -12,6 +12,9 @@ function $$(selector, context = document) {
 let pages = [
     {url: '', title: 'Home'}, 
     {url: 'projects/', title: 'Projects'},
+    {url: 'contact/', title: 'Contact'}, 
+    {url: 'https://github.com/rluutuyen-arch/', title: 'Github'}, 
+    {url: 'resume.html', title: 'Resume'}
 ]
 
 const BASE_PATH = (location.hostname === "localhost" || location.hostname === "127.0.0.1")
@@ -24,8 +27,6 @@ document.body.prepend(nav);
 for(let p of pages) {
     let url = p.url;
     let title = p.title
-    if (!url.startsWith('http')) {
-        url = BASE_PATH + url;
-    }
+    url = !url.startsWith('http') ? BASE_PATH + url : url;
     nav.insertAdjacentHTML('beforeend', `<a href="${url}">${title}</a>`);
 }
