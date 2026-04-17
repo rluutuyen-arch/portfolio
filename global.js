c44655fcecef6fc35f27639fb07d16723f0e8539
sleep1bo1 <rluutuyen@ucsd.edu>
2026-04-17 12:22:08 -0700

    3.1 fix 4.0

--- global.js
+++ global.js
@@ -17,10 +17,9 @@ let pages = [
     {url: 'resume.html', title: 'Resume'}
 ]
 
-const BASE_PATH = 
-    (location.hostname ==="localhost" || location.hostname === "127.0.0.1") 
-        ? "/" 
-        : "/website/";
+const BASE_PATH = (location.hostname ==="localhost" || location.hostname === "127.0.0.1") 
+    ? "/" 
+    : "/website/";
 
 let nav = document.createElement('nav');
 document.body.prepend(nav);
@@ -29,7 +28,5 @@ for(let p of pages) {
     let url = p.url;
     let title = p.title
     url = !url.startsWith('http') ? BASE_PATH + url : url;
-    nav.insertAdjacentHTML(
-        'beforeend', 
-        '<a href="${url}">${title}</a>');
+    nav.insertAdjacentHTML('beforeend', `<a href="${url}">${title}</a>`);
 }
