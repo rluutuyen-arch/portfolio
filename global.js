822a224749888984409b2a1092437f8e6017e81d
sleep1bo1 <rluutuyen@ucsd.edu>
2026-04-17 12:46:14 -0700

    AUGH

--- global.js
+++ global.js
@@ -16,7 +16,7 @@ let pages = [
 
 const BASE_PATH = (location.hostname === "localhost" || location.hostname === "127.0.0.1")
   ? "/"                  
-  : "/portfolio/";         
+  : "/website/";         
 
 let nav = document.createElement('nav');
 document.body.prepend(nav);
