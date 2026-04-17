fd586dd3e2002655f3dc336444b39c7e18813930
sleep1bo1 <rluutuyen@ucsd.edu>
2026-04-17 13:01:36 -0700

    3.2

--- global.js
+++ global.js
@@ -28,5 +28,10 @@ for(let p of pages) {
     let url = p.url;
     let title = p.title
     url = !url.startsWith('http') ? BASE_PATH + url : url;
-    nav.insertAdjacentHTML('beforeend', `<a href="${url}">${title}</a>`);
+    let a = document.createElement('a');
+    a.href = url;
+    a.textContent = title;
+    nav.append(a);
+    a.classList.toggle('current', a.host === location.host && a.pathname === location.pathname,)
+    a.classList.toggle('_blank', a.host !== location.host)
 }
