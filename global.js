83b9e8770cd9ee9a570da9e9d55e506896e6b3e5
sleep1bo1 <rluutuyen@ucsd.edu>
2026-04-17 12:15:29 -0700

    Up to 3.1

---
+++ global.js
@@ -0,0 +1,33 @@
+console.log('IT’S ALIVE!');
+
+function $$(selector, context = document) {
+  return Array.from(context.querySelectorAll(selector));
+}
+
+// let navLinks = $$("nav a");
+// let currentLink = navLinks.find(
+//     (a) => a.host === location.host && a.pathname === location.pathname
+// );
+// currentLink?.classlist.add("current")
+let pages = [
+    {url: '', title: 'Home'}, 
+    {url: 'projects/', title: 'Projects'}, 
+    {url: 'contact/', title: 'Contact'}, 
+    {url: 'https://github.com/rluutuyen-arch/', title: 'Github'}, 
+    {url: 'resume.html', title: 'Resume'}
+]
+
+const BASE_PATH = 
+    (location.hostname ==="localhost" || location.hostname === "127.0.0.1") 
+        ? "/" 
+        : "website/";
+
+let nav = document.createElement('nav');
+document.body.prepend(nav);
+
+for(let p of pages) {
+    let url = p.url;
+    let title = p.title
+    url = !url.startsWith('http') ? + url:url;
+    nav.insertAdjacentHTML('beforeend', '<a href="${url}">${title}</a>');
+}
