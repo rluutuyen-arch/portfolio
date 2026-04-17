eb730a1dd7f21ba0275d5da51666f67176cd7e1e
sleep1bo1 <rluutuyen@ucsd.edu>
2026-04-17 12:27:40 -0700

    3.1 test 5.9

--- global.js
+++ global.js
@@ -11,10 +11,10 @@ function $$(selector, context = document) {
 // currentLink?.classlist.add("current")
 let pages = [
     {url: '', title: 'Home'}, 
-    {url: 'projects/', title: 'Projects'}, 
-    {url: 'contact/', title: 'Contact'}, 
-    {url: 'https://github.com/rluutuyen-arch/', title: 'Github'}, 
-    {url: 'resume.html', title: 'Resume'}
+    {url: 'projects/', title: 'Projects'}
+    // {url: 'contact/', title: 'Contact'}, 
+    // {url: 'https://github.com/rluutuyen-arch/', title: 'Github'}, 
+    // {url: 'resume.html', title: 'Resume'}
 ]
 
 const BASE_PATH = (location.hostname ==="localhost" || location.hostname === "127.0.0.1") 
@@ -27,6 +27,6 @@ document.body.prepend(nav);
 for(let p of pages) {
     let url = p.url;
     let title = p.title
-    //url = !url.startsWith('http') ? BASE_PATH + url : url;
+    url = !url.startsWith('http') ? BASE_PATH + url : url;
     nav.insertAdjacentHTML('beforeend', `<a href="${url}">${title}</a>`);
 }
