37e5bd835edcc0b7ec3a0e9412c0266ee815200e
sleep1bo1 <rluutuyen@ucsd.edu>
2026-04-17 14:18:49 -0700

    part 4

--- global.js
+++ global.js
@@ -34,7 +34,7 @@ for(let p of pages) {
     nav.append(a);
     a.classList.toggle('current', a.host === location.host && a.pathname === location.pathname,)
     a.toggleAttribute('target', a.host !== location.host);
-    console.log(a.classList)
+    a.target = a.host !== location.host ? "_blank" : "";
 }
 
 document.body.insertAdjacentHTML(
