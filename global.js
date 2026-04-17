d753037253033a8fa97203b825853fa65fb6052f
sleep1bo1 <rluutuyen@ucsd.edu>
2026-04-17 12:25:17 -0700

    3.1 test 5.8

--- global.js
+++ global.js
@@ -25,9 +25,8 @@ let nav = document.createElement('nav');
 document.body.prepend(nav);
 
 for(let p of pages) {
-    console.log(p)
     let url = p.url;
     let title = p.title
-    url = !url.startsWith('http') ? BASE_PATH + url : url;
+    //url = !url.startsWith('http') ? BASE_PATH + url : url;
     nav.insertAdjacentHTML('beforeend', `<a href="${url}">${title}</a>`);
 }
