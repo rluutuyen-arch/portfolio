9f2e1b68de8d18a45d985c803c3084cfcc4cdfda
sleep1bo1 <rluutuyen@ucsd.edu>
2026-04-24 14:32:14 -0700

    title project count

--- projects/projects.js
+++ projects/projects.js
@@ -2,6 +2,6 @@ import { fetchJSON, renderProjects } from '../global.js';
 const projects = await fetchJSON('../lib/projects.json');
 const projectsContainer = document.querySelector('.projects');
 renderProjects(projects, projectsContainer, 'h2');
-const title = document.querySelector("project-title");
+const title = document.querySelector('.project-title');
 const count = projects.length;
 title.textContent = "(${count}) Projects"
