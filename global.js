09f70d8e7ee5538f29ce3f9936869edb26247298
sleep1bo1 <rluutuyen@ucsd.edu>
2026-04-24 23:31:37 -0700

    fixed??????

--- global.js
+++ global.js
@@ -97,7 +97,7 @@ export function renderProjects(projects, containerElement, headingLevel = 'h2') 
     projects.forEach(project => { 
         const article = document.createElement('article');
         article.innerHTML = `
-            <headingLevel>${project.title}</headingLevel>
+            <${headingLevel}>${project.title}</${headingLevel}>
             <img src="/${project.image}" alt="${project.title}">
             <p>${project.description}</p>
         `;
