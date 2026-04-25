88c8f727f7898be968b9da988f80817d23565dc1
sleep1bo1 <rluutuyen@ucsd.edu>
2026-04-24 23:42:10 -0700

    testing

--- global.js
+++ global.js
@@ -96,6 +96,7 @@ export function renderProjects(projects, containerElement, headingLevel = 'h2') 
 
     projects.forEach(project => { 
         const article = document.createElement('article');
+        console.log(project.image)
         article.innerHTML = `
             <${headingLevel}>${project.title}</${headingLevel}>
             <img src="${project.image}" alt="${project.title}">
