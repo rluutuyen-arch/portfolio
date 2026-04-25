f2ea304f57ba695c1765037ae0b7ea05e7530d5b
sleep1bo1 <rluutuyen@ucsd.edu>
2026-04-24 22:09:23 -0700

    Part 2

--- index.js
+++ index.js
@@ -2,3 +2,4 @@ import { fetchJSON, renderProjects, fetchGithubData } from './global.js';
 const projects = await fetchJSON('./lib/projects.json');
 const latestProjects = projects.slice(0, 3);
 const projectsContainer = document.querySelector('.projects');
+renderProjects(latestProjects, projectsContainer, 'h2');
--- projects/projects.js
+++ projects/projects.js
@@ -2,7 +2,7 @@ import { fetchJSON, renderProjects } from '../global.js';
 const projects = await fetchJSON('../lib/projects.json');
 const projectsContainer = document.querySelector('.projects');
 renderProjects(projects, projectsContainer, 'h2');
-const title = document.querySelector('.project-title');
+const title = document.querySelector('.projects-title');
 const count = projects.length;
 console.log(Object.keys(title))
 title.textContent = "(${count}) Projects"
