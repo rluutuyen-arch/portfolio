12b6f4f34db7cb7d3a69cebdf19bb65e91f3799e
Ryan Luutuyen <rluutuyen@ucsd.edu>
2026-04-29 13:41:38 -0700

    Step 1.3 #1

--- projects/projects.js
+++ projects/projects.js
@@ -6,3 +6,11 @@ const title = document.querySelector('.projects-title');
 if (title && projects) {
     title.textContent = `${projects.length} Projects`
 }
+
+import * as d3 from 'https://cdn.jsdelivr.net/npm/d3@7.9.0/+esm';
+let arcGenerator = d3.arc().innerRadius(0).outerRadius(50);
+let arc = arcGenerator({
+  startAngle: 0,
+  endAngle: 2 * Math.PI,
+});
+d3.select('svg').append('path').attr('d', arc).attr('fill', 'red');
