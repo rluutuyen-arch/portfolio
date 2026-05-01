c0d694a2cc23bc6337e7f9beaa94b7ecd66e9cc4
sleep1bo1 <rluutuyen@ucsd.edu>
2026-04-30 21:56:04 -0700

    Part 1 All 1

--- projects/projects.js
+++ projects/projects.js
@@ -8,9 +8,18 @@ if (title && projects) {
 }
 
 import * as d3 from 'https://cdn.jsdelivr.net/npm/d3@7.9.0/+esm';
-let arcGenerator = d3.arc().innerRadius(0).outerRadius(50);
-let arc = arcGenerator({
-  startAngle: 0,
-  endAngle: 2 * Math.PI,
+// let arcGenerator = d3.arc().innerRadius(0).outerRadius(50);
+// let arc = arcGenerator({
+//   startAngle: 0,
+//   endAngle: 2 * Math.PI,
+// });
+// d3.select('svg').append('path').attr('d', arc).attr('fill', 'red');
+
+let data = [1, 2, 3, 4, 5, 5];
+let colors = d3.scaleOrdinal(d3.schemeTableau10);
+let sliceGenerator = d3.pie();
+let arcData = sliceGenerator(data);
+let arcs = arcData.map((d) => arcGenerator(d));
+arcs.forEach((arc, idx) => {
+    d3.select('svg').append('path').attr('d', arc).attr('fill', colors);
 });
-d3.select('svg').append('path').attr('d', arc).attr('fill', 'red');
