d4b8006d66f79479eee11f348283607f0187f855
sleep1bo1 <rluutuyen@ucsd.edu>
2026-04-30 22:07:50 -0700

    Step 2.1-2.2 & step pie chart fix

--- projects/projects.js
+++ projects/projects.js
@@ -8,18 +8,27 @@ if (title && projects) {
 }
 
 import * as d3 from 'https://cdn.jsdelivr.net/npm/d3@7.9.0/+esm';
-// let arcGenerator = d3.arc().innerRadius(0).outerRadius(50);
-// let arc = arcGenerator({
-//   startAngle: 0,
-//   endAngle: 2 * Math.PI,
-// });
-// d3.select('svg').append('path').attr('d', arc).attr('fill', 'red');
 
-let data = [1, 2, 3, 4, 5, 5];
+let data = [
+  { value: 1, label: 'apples' },
+  { value: 2, label: 'oranges' },
+  { value: 3, label: 'mangos' },
+  { value: 4, label: 'pears' },
+  { value: 5, label: 'limes' },
+  { value: 5, label: 'cherries' },
+];
 let colors = d3.scaleOrdinal(d3.schemeTableau10);
-let sliceGenerator = d3.pie();
+let sliceGenerator = d3.pie().value((d) => d.value);
 let arcData = sliceGenerator(data);
 let arcs = arcData.map((d) => arcGenerator(d));
 arcs.forEach((arc, idx) => {
-    d3.select('svg').append('path').attr('d', arc).attr('fill', colors);
+    d3.select('svg').append('path').attr('d', arc).attr('fill', colors[idx]);
+});
+
+let legend = d3.select('.legend');
+data.forEach((d, idx) => {
+  legend
+    .append('li')
+    .attr('style', `--color:${colors(idx)}`) 
+    .html(`<span class="swatch"></span> ${d.label} <em>(${d.value})</em>`); 
 });
